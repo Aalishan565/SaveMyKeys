fa0d418ea25e242739b472501ef2f7d66b97ef3b
Aalishan Ansari <aalishan565@gmail.com>
2019-11-05 11:09:47 +0530

    -added ktx lib

--- app/src/main/java/com/savemykeys/utils/AppSharedPreference.kt
+++ app/src/main/java/com/savemykeys/utils/AppSharedPreference.kt
@@ -1,6 +1,7 @@
 package com.savemykeys.utils
 
 import android.content.Context
+import androidx.core.content.edit
 
 
 class AppSharedPreference {
@@ -12,41 +13,38 @@ class AppSharedPreference {
             get() {
                 if (appSharedPreference == null)
                     appSharedPreference = AppSharedPreference()
-
                 return appSharedPreference!!
 
             }
     }
 
     fun saveStringPref(context: Context, key: String, value: String) {
-        val sharedpreferences =
+        val sharedPreferences =
             context.getSharedPreferences(Constants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val editor = sharedpreferences.edit()
-        editor.putString(key, value)
-        editor.commit()
+        sharedPreferences.edit(true) { putString(key, value) }
     }
 
     fun getStringPref(context: Context, key: String): String? {
-        val sharedpreferences =
+        val sharedPreferences =
             context.getSharedPreferences(Constants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        return sharedpreferences.getString(key, null)
+        return sharedPreferences.getString(key, null)
     }
 
     fun saveBooleanPref(context: Context, key: String, value: Boolean) {
-        val sharedpreferences =
+        val sharedPreferences =
             context.getSharedPreferences(Constants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val editor = sharedpreferences.edit()
-        editor.putBoolean(key, value)
-        editor.commit()
+        sharedPreferences.edit(true) { putBoolean(key, value) }
+
+        /* val editor = sharedpreferences.edit()
+         editor.putBoolean(key, value)
+         editor.commit()*/
     }
 
 
     fun clearAllPrefs(context: Context) {
-        val sharedpreferences =
+        val sharedPreferences =
             context.getSharedPreferences(Constants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE)
-        val editor = sharedpreferences.edit()
-        editor.clear()
-        editor.commit()
+        sharedPreferences.edit(true) { clear() }
     }
 
     fun getBooleanPref(context: Context, key: String): Boolean {
@@ -54,6 +52,5 @@ class AppSharedPreference {
             context.getSharedPreferences(Constants.APP_PREFERENCE_NAME, Context.MODE_PRIVATE)
         return sharedpreferences.getBoolean(key, false)
     }
-
 
 }
