da24f6cc91f8b3d0dfb1ae7f0644ed60e3607418
Aalishan <aalishan565@gmail.com>
2020-01-11 01:26:14 +0530

    -UI fixes

--- app/src/main/java/com/savemykeys/utils/Constants.kt
+++ app/src/main/java/com/savemykeys/utils/Constants.kt
@@ -6,6 +6,6 @@ object Constants {
     const val APP_DB_NAME = "save_my_key_db"
     const val SINGLE_RECORD = "singleRecord"
     const val SINGLE_RECORD_SCREEN_TITLE = "singleRecordScreenTitle"
-    const val SPLASH_TIME_OUT:Long = 500
+    const val SPLASH_TIME_OUT:Long = 1000
 
 }
