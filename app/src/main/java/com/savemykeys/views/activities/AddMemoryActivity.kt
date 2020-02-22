8343f08bb243c3a3435260b275b2580e366ae0da
Aalishan <aalishan565@gmail.com>
2020-02-22 16:26:35 +0530

    -ui for add and reminder flow

--- app/src/main/java/com/savemykeys/views/activities/AddMemoryActivity.kt
+++ app/src/main/java/com/savemykeys/views/activities/AddMemoryActivity.kt
@@ -1,14 +1,20 @@
 package com.savemykeys.views.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.savemykeys.R
+import com.savemykeys.utils.AppUtils
 import com.savemykeys.utils.Constants
 import kotlinx.android.synthetic.main.activity_add_memory.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddMemoryActivity : AppCompatActivity() {
-
+    private val myCalendar = Calendar.getInstance()
     private val TAG = "AddMemoryActivity"
     private var screenTitle: String? = null
 
@@ -29,5 +35,32 @@ class AddMemoryActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp)
+        etMemoryDate.setOnClickListener { v ->
+            AppUtils.hideKeyboard(this, v)
+            val datePickerDialog = DatePickerDialog(
+                this, date, myCalendar.get(
+                    Calendar.YEAR
+                ),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.datePicker.maxDate = System.currentTimeMillis()
+            datePickerDialog.show()
+        }
     }
+
+    var date =
+        OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            myCalendar[Calendar.YEAR] = year
+            myCalendar[Calendar.MONTH] = monthOfYear
+            myCalendar[Calendar.DAY_OF_MONTH] = dayOfMonth
+            updateUI()
+        }
+
+    private fun updateUI() {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        etMemoryDate.setText(sdf.format(myCalendar.time))
+    }
+
 }
--- app/src/main/java/com/savemykeys/views/activities/AddReminderActivity.kt
+++ app/src/main/java/com/savemykeys/views/activities/AddReminderActivity.kt
@@ -1,14 +1,19 @@
 package com.savemykeys.views.activities
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.savemykeys.R
+import com.savemykeys.utils.AppUtils
 import com.savemykeys.utils.Constants
-import kotlinx.android.synthetic.main.activity_reminder.*
+import kotlinx.android.synthetic.main.activity_add_reminder.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddReminderActivity : AppCompatActivity() {
-
+    private val myCalendar = Calendar.getInstance()
     private val TAG = "AddReminderActivity"
     private var screenTitle: String? = null
 
@@ -20,14 +25,40 @@ class AddReminderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminder)
+        setContentView(R.layout.activity_add_reminder)
         Log.d(TAG, "onCreate()")
         val bundle = intent.extras
-        screenTitle = bundle?.getString(Constants.ADD_REMINDER_SCREEN_TITLE)
+        screenTitle = bundle?.getString(Constants.ADD_MEMORY_SCREEN_TITLE)
         toolbarAddReminder.title = screenTitle
         setSupportActionBar(toolbarAddReminder)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp)
+        etReminderDate.setOnClickListener { v ->
+            AppUtils.hideKeyboard(this, v)
+            val datePickerDialog = DatePickerDialog(
+                this, date, myCalendar.get(
+                    Calendar.YEAR
+                ),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            )
+            datePickerDialog.datePicker.minDate = System.currentTimeMillis()
+            datePickerDialog.show()
+        }
+    }
+
+    var date =
+        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            myCalendar[Calendar.YEAR] = year
+            myCalendar[Calendar.MONTH] = monthOfYear
+            myCalendar[Calendar.DAY_OF_MONTH] = dayOfMonth
+            updateUI()
+        }
+
+    private fun updateUI() {
+        val myFormat = "dd-MM-yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        etReminderDate.setText(sdf.format(myCalendar.time))
     }
 }
